5e17e70df1f6c9ede62f0e36b504a39c7f074588
rhodinemma <rhodinemma10@gmail.com>
2023-06-07 22:19:44 +0300

    feat: add users model and necessary user actions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import http from 'http'
-import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
@@ -17,7 +16,7 @@ app.use(cors({
 
 app.use(compression())
 app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(express.json())
 
 const server = http.createServer(app)
 
