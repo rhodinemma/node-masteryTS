94c76ae13e411f030d04c34fc58a98812fc727eb
rhodinemma <rhodinemma10@gmail.com>
2023-06-08 21:38:56 +0300

    feat: authenticate routes using cookies

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 
 import { getAllUsers } from '../controllers/users'
+import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers);
 }
