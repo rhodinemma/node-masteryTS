dbc34f5b6c33e755d697458c3df8364ad0aa8eb7
rhodinemma <rhodinemma10@gmail.com>
2023-06-08 21:48:46 +0300

    feat: add logic to delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,12 +1,24 @@
 import express from 'express'
 
-import { getUsers } from '../database/users'
+import { deleteUserById, getUsers } from '../database/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
 
         return res.status(200).json(users)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400)
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const deletedUser = await deleteUserById(id)
+
+        return res.json(deletedUser)
     } catch (error) {
         console.log(error)
         return res.sendStatus(400)
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,26 @@ import { get, merge } from 'lodash'
 
 import { getUserBySessionToken } from '../database/users'
 
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(403)
+        }
+
+        if (currentUserId.toString() != id) {
+            return res.sendStatus(403)
+        }
+
+        next();
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(403)
+    }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['RHODIN-AUTH'];
