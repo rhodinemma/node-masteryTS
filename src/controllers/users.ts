405fa28aaa51c4e353f40b2892166b6b8147ff5f
rhodinemma <rhodinemma10@gmail.com>
2023-06-08 21:53:08 +0300

    feat: add logic to update a user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { deleteUserById, getUsers } from '../database/users'
+import { deleteUserById, getUserById, getUsers } from '../database/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -19,6 +19,27 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const deletedUser = await deleteUserById(id)
 
         return res.json(deletedUser)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400)
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if (!username) {
+            return res.sendStatus(400)
+        }
+
+        const user = await getUserById(id);
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json(user).end();
     } catch (error) {
         console.log(error)
         return res.sendStatus(400)
