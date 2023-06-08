fa8fd09bd86da9784d97487ec5979e7670111175
rhodinemma <rhodinemma10@gmail.com>
2023-06-08 21:22:25 +0300

    feat: add logic to authenticate users

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,6 +1,7 @@
-import { register } from '../controllers/authentication'
+import { login, register } from '../controllers/authentication'
 import express from 'express'
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
 }
